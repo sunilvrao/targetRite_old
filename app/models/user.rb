b327e1684b7e285aa8159c118ae85d7422919982
Sunil Rao <sunil2rao@gmail.com>
2013-10-30 11:13:15 -0500

    initial project

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :role_ids, :as => :admin
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  :remember_me
+end
